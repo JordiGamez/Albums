7564b9a3723bb3be372acab1cd8e290155c7ec46
Jordi Gamez <jordigamezllorach@gmail.com>
2018-10-03 17:51:15 +0200

    Initial Commit

---
+++ Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by Jordi Gamez on 03/10/2018.
+//  Copyright © 2018 Jordi Gamez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
